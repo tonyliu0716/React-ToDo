9baef87bb316227fc32b2ccf39d1aa3e4e9ee797
tonyliu0716 <tonyliu0716@gmail.com>
2019-01-01 20:44:14 -0600

    Add test cases for Todo and TodoList

---
+++ app/tests/components/Todo.test.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var $ = require('jquery');
+var expect = require('expect');
+
+
+var Todo = require('Todo');
+
+describe('Todo', () => {
+
+    it('should exist', () => {
+        expect(Todo).toExist();
+    });
+
+});
