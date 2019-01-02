5b125f70a8784dbf8db21ccd728ad0fbf572e999
tonyliu0716 <tonyliu0716@gmail.com>
2019-01-01 19:54:25 -0600

    Create the app container

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+
+    render: function() {
+        return(
+            <div>ToDo APP</div>
+        );
+
+    }
+
+});
+
+module.exports = TodoApp;
